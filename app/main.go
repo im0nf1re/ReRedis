84aee1e0b7f5450f25a1c29810db5591da12a1cf
imonfire <imonfire.prod@gmail.com>
2026-01-11 23:57:19 +0500

    multithreading clients handling

--- app/main.go
+++ app/main.go
@@ -4,37 +4,49 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "127.0.0.1:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		panic(err)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer l.Close()
 
-	readBuf := make([]byte, 0)
 	for {
-		_, err = conn.Read(readBuf)
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error handling connection:", err)
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	readBuf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(readBuf)
+
+		if err == io.EOF {
+			fmt.Println("Client disconnected")
+			return
+		}
+
 		if err != nil {
 			fmt.Println("Error reading request: ", err.Error())
-			os.Exit(1)
+			return
 		}
-		if err == io.EOF {
-			break
+
+		if n == 0 {
+			continue
 		}
 
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			fmt.Println("Error responding: ", err.Error())
-			os.Exit(1)
+			return
 		}
 	}
-
 }
